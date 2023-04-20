27415da4a9378a60d69ce4c50df631c76dad27cc
Michael Chirillo <michael.a.chirillo@gmail.com>
2023-04-20 12:20:23 +0200

    Add readme

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,11 @@
 from src import my_one_function
 
-def test_main():
+def test_main_hello():
     assert my_one_function("hello") == "hello"
 
+# def test_main_nerp():
+#     assert my_one_function("nerp") == "hello"
+
 if __name__ == "__main__":
-    test_main()
+    test_main_hello()
+#    test_main_nerp()
